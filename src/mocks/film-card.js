7e7f8cb6ce3c77769390289fdf2a9bdac0b27833
nothingvrs <b00M_vrs@mail.ru>
2019-12-03 22:03:46 +0300

    Подправил пути к картнкам

--- src/mocks/film-card.js
+++ src/mocks/film-card.js
@@ -24,13 +24,13 @@ const titles = [
 ];
 
 const posters = [
-  `../images/posters/made-for-each-other.png`,
-  `../images/posters/popeye-meets-sinbad.png`,
-  `../images/posters/sagebrush-trail.jpg`,
-  `../images/posters/santa-claus-conquers-the-martians.jpg`,
-  `../images/posters/the-dance-of-life.jpg`,
-  `../images/posters/the-great-flamarion.jpg`,
-  `../images/posters/the-man-with-the-golden-arm.jpg`,
+  `./images/posters/made-for-each-other.png`,
+  `./images/posters/popeye-meets-sinbad.png`,
+  `./images/posters/sagebrush-trail.jpg`,
+  `./images/posters/santa-claus-conquers-the-martians.jpg`,
+  `./images/posters/the-dance-of-life.jpg`,
+  `./images/posters/the-great-flamarion.jpg`,
+  `./images/posters/the-man-with-the-golden-arm.jpg`,
 ];
 
 const actors = [
